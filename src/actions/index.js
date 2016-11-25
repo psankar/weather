31fa1e142ebff739e9965b109acb0617525f5841
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-25 16:53:18 +0530

    57 ajax requests with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '3bb1f99b6b7db274fdae57b675aa86a6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+    url = `${ROOT_URL}&q=${city},us`;
+    request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
