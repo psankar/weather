f3d61b85bfdb84f86dee0d670cba545c0aba5832
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-26 16:34:41 +0530

    58 promise in practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,8 +7,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 
-    url = `${ROOT_URL}&q=${city},us`;
-    request = axios.get(url);
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
 
     return {
         type: FETCH_WEATHER,
